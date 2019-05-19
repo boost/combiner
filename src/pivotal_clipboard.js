ee2a7fc49f32769e7aeff60fa7527fbe40f4a6cd
Paul Mesnilgrente <web@paul-mesnilgrente.com>
2019-05-19 20:13:49 +1200

    able to copy data to clipboard when story fetched by api

--- src/pivotal_clipboard.js
+++ src/pivotal_clipboard.js
@@ -1,14 +1,55 @@
 import './pivotal.scss';
-const Clipboard = require('clipboard');
 var $ = require('jquery');
 
+// This function must be called in a visible page, such as a browserAction popup
+// or a content script. Calling it in a background page has no effect!
+function copyToClipboard(text, html) {
+    function oncopy(event) {
+        document.removeEventListener("copy", oncopy, true);
+        // Hide the event from the page to prevent tampering.
+        event.stopImmediatePropagation();
+
+        // Overwrite the clipboard content.
+        event.preventDefault();
+        event.clipboardData.setData("text/plain", text);
+        event.clipboardData.setData("text/html", html);
+    }
+    document.addEventListener("copy", oncopy, true);
+
+    // Requires the clipboardWrite permission, or a user gesture:
+    document.execCommand("copy");
+}
+
+function fetchStory(storyId) {
+  return fetch('https://www.pivotaltracker.com/services/v5/stories/165404870')
+  .then((response) => { return response.json(); });
+}
+
+function copyTitle() {
+  const storyId = $(this).attr('data-id');
+  fetchStory(storyId).then((story) => {
+    copyToClipboard(story.name);
+  });
+}
+
+function copyR4A() {
+  const storyId = $(this).attr('data-id');
+  fetchStory(storyId).then((story) => {
+    debugger;
+    copyToClipboard(story.description);
+  });
+}
 
 function runClipboard($stories) {
   $stories.each(function(index) {
+    const storyId = $(this).attr('data-id');
     const storyTitle = $(this).find('span[data-aid="StoryPreviewItem__title"]').text();
-    const button = `<button class="copy-title" data-clipboard-text="${storyTitle}">Copy title</button>`
-    $(this).find('header.preview').after(button);
-    new Clipboard('.copy-title');
+    const buttonTitle = `<button data-id="${storyId}" class="copy-title">T</button>`
+    const buttonR4A   = `<button data-id="${storyId}" class="copy-r4a">r4a</button>`
+    $(this).find('header.preview').after(buttonTitle);
+    $(this).find('header.preview').after(buttonR4A);
+    $(this).find('.copy-title').click(copyTitle);
+    $(this).find('.copy-r4a').click(copyR4A);
   });
 }
 
@@ -17,11 +58,9 @@ $(function() {
     let $stories = $('.story.model');
 
     if (!$stories.length) {
-      console.log('waitStoryLoading');
       return setTimeout(waitStoryLoading, 250);
     }
 
-    console.log('runClipboard');
     runClipboard($stories);
   }
   waitStoryLoading();
