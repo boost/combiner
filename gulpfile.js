6d6799ee207a89d55b97525c1908dc145d0a148b
Paul Mesnilgrente <web@paul-mesnilgrente.com>
2019-05-26 18:08:51 +1200

    remove gulp to use only webpack

--- gulpfile.js
+++ gulpfile.js
@@ -1,74 +1,74 @@
-const { src, dest, watch, series, parallel } = require('gulp');
-const del         = require('del');
-const terser      = require('gulp-terser');
-const gulpZip     = require('gulp-zip');
-const runSequence = require('run-sequence');
-const path        = require('path');
-const webpack     = require('webpack');
-var webpackConfig = require('./webpack.config.js');
+// const { src, dest, watch, series, parallel } = require('gulp');
+// const del         = require('del');
+// const terser      = require('gulp-terser');
+// const gulpZip     = require('gulp-zip');
+// const runSequence = require('run-sequence');
+// const path        = require('path');
+// const webpack     = require('webpack');
+// var webpackConfig = require('./webpack.config.js');
 
-const copyGlobs = ['assets/*', 'src/background/basecamp/context.js'];
-const localesGlob = '_locales/**/*';
-const distFolder = 'dist';
+// const copyGlobs = ['assets/*', 'src/background/basecamp/context.js'];
+// const localesGlob = '_locales/**/*';
+// const distFolder = 'dist';
 
-function clean() {
-  return del(['dist', '*.zip']);
-}
+// function clean() {
+//   return del(['dist', '*.zip']);
+// }
 
-function webpackIt(cb) {
-  webpack(webpackConfig, function (err, stats) {
-    if (err) throw new gutil.PluginError('[webpack]', err);
-    console.log(stats.toString());
-    cb();
-  });
-}
+// function webpackIt(cb) {
+//   webpack(webpackConfig, function (err, stats) {
+//     if (err) throw new gutil.PluginError('[webpack]', err);
+//     console.log(stats.toString());
+//     cb();
+//   });
+// }
 
-function webpackWatch(cb) {
-  webpackConfig.watch = true;
-  webpackConfig.mode = 'development';
-  webpack(webpackConfig, function (err, stats) {
-    if (err) throw new gutil.PluginError('[webpack]', err);
-    console.log(stats.toString());
-    cb();
-  });
-}
+// function webpackWatch(cb) {
+//   webpackConfig.watch = true;
+//   webpackConfig.mode = 'development';
+//   webpack(webpackConfig, function (err, stats) {
+//     if (err) throw new gutil.PluginError('[webpack]', err);
+//     console.log(stats.toString());
+//     cb();
+//   });
+// }
 
-function uglifyPivotal() {
-  return src('dist/pivotal.js')
-    .pipe(terser())
-    .pipe(dest('dist'));
-}
+// function uglifyPivotal() {
+//   return src('dist/pivotal.js')
+//     .pipe(terser())
+//     .pipe(dest('dist'));
+// }
 
-function uglifyBasecamp() {
-  return src('dist/basecamp.js')
-    .pipe(terser())
-    .pipe(dest('dist'));
-}
+// function uglifyBasecamp() {
+//   return src('dist/basecamp.js')
+//     .pipe(terser())
+//     .pipe(dest('dist'));
+// }
 
-function copyLocales() {
-  return src([localesGlob], {base: '.'})
-  .pipe(dest(distFolder))
-}
+// function copyLocales() {
+//   return src([localesGlob], {base: '.'})
+//   .pipe(dest(distFolder))
+// }
 
-function copy() {
-  return src(copyGlobs)
-    .pipe(dest(distFolder));
-}
+// function copy() {
+//   return src(copyGlobs)
+//     .pipe(dest(distFolder));
+// }
 
-function zip() {
-  return src('dist/**/*')
-    .pipe(gulpZip('boost-browser-extension.zip'))
-    .pipe(dest('.'));
-}
+// function zip() {
+//   return src('dist/**/*')
+//     .pipe(gulpZip('boost-browser-extension.zip'))
+//     .pipe(dest('.'));
+// }
 
-function copyLocalesWatch() {
-  watch(localesGlob, copyLocales);
-}
+// function copyLocalesWatch() {
+//   watch(localesGlob, copyLocales);
+// }
 
-function copyWatch() {
-  watch(copyGlobs, copy);
-}
+// function copyWatch() {
+//   watch(copyGlobs, copy);
+// }
 
-exports.default = series(clean, webpackIt, copy, copyLocales);
-exports.build = series(exports.default, uglifyPivotal, uglifyBasecamp, zip);
-exports.watch = series(exports.default, parallel(webpackWatch, copyWatch, copyLocalesWatch));
+// exports.default = series(clean, webpackIt, copy, copyLocales);
+// exports.build = series(exports.default, uglifyPivotal, uglifyBasecamp, zip);
+// exports.watch = series(exports.default, parallel(webpackWatch, copyWatch, copyLocalesWatch));
--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,26 @@
 const path = require('path');
-var WebpackNotifierPlugin = require('webpack-notifier');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const WebpackNotifierPlugin = require('webpack-notifier');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+if (process.env.NODE_ENV == null) {
+    process.env.NODE_ENV = 'development';
+}
+const ENV = process.env.ENV = process.env.NODE_ENV;
 
 module.exports = {
   entry: {
-    pivotal: './src/content/pivotal/js/index.js',
-    basecamp: './src/content/basecamp/index.js'
+    'content/pivotal':    './src/content/pivotal/js/index.js',
+    'content/basecamp':   './src/content/basecamp/index.js',
+    'background/basecamp/context': './src/background/basecamp/context.js',
+    'popup/main':         './src/popup/main.js'
   },
   output: {
     filename: '[name].js',
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, 'build')
   },
-  mode: 'production',
+  mode: ENV,
   module: {
     rules: [{
       test: /\.scss$/,
@@ -18,9 +28,33 @@ module.exports = {
     },{
       test: /\.png$/,
       loaders: ['url-loader']
+    },{
+      test: /\.(html)$/,
+      loader: 'html-loader',
+    },{
+      test: /\.jsx$/,
+      exclude: /(node_modules|bower_components)/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env']
+        }
+      }
     }]
   },
   plugins: [
-    new WebpackNotifierPlugin()
+    new WebpackNotifierPlugin(),
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: './src/popup/index.html',
+      filename: 'popup/index.html',
+      chunks: ['popup/vendor', 'popup/main'],
+    }),
+    new CopyWebpackPlugin([
+        './src/manifest.json',
+        { from: './src/_locales', to: '_locales' },
+        { from: './src/images', to: 'images' },
+        { from: './src/popup/images', to: 'popup/images' },
+    ]),
   ]
 };
