a75007473adc74da513be548b640c40840383974
Paul Mesnilgrente <web@paul-mesnilgrente.com>
2019-09-18 09:45:58 +1200

    resizing the textarea after including the val makes a better experience
    it avoids to have to input a random character to start interacting with it

--- src/content/gitlab/index.js
+++ src/content/gitlab/index.js
@@ -1,7 +1,15 @@
 import { buildTitle, buildMessage } from 'utils/pull_request';
+import $ from 'jquery';
 import browser from 'webextension-polyfill';
 
 browser.runtime.onMessage.addListener(story => {
-  document.getElementById('merge_request_title').value = buildTitle(story);
-  document.getElementById('merge_request_description').value = buildMessage(story);
+  $('#merge_request_title').val(buildTitle(story));
+  $('#merge_request_description').val(buildMessage(story));
+
+  // trigger a resizing of the textarea to include the whole text
+  let script = document.createElement('script');
+  script.type = 'text/javascript';
+  script.async = true;
+  script.textContent = `$('#merge_request_description').trigger('change');`;
+  $('head').append(script);
 });
