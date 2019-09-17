338b872fe77e26cabcb88cb3c4c4f70dfcc38380
Paul Mesnilgrente <web@paul-mesnilgrente.com>
2019-09-18 09:45:03 +1200

    waiting for the stories in the index avoids to repeat the operation in different scripts

--- src/content/pivotal/js/add_story.js
+++ src/content/pivotal/js/add_story.js
@@ -2,38 +2,41 @@ import browser from 'webextension-polyfill';
 import snippetInput from './utils/snippet_input';
 import $ from 'jquery';
 
-function waitForDescriptionTextarea() {
-  let $textarea = $('.edit.details.new textarea[placeholder="Add a description"]');
+const waitForDescriptionTextarea = () => {
+  const $textarea = $('.edit.details.new textarea[placeholder="Add a description"]');
 
   if (!$textarea.length) {
     setTimeout(waitForDescriptionTextarea, 250);
   }
 
   snippetInput($textarea, browser.i18n.getMessage('storyDescriptionTemplate'));
-}
+  $textarea.trigger('focus');
+};
 
-function waitForNewStory() {
-  let $story = $('.edit.details.new');
+const waitForNewStory = () => {
+  const $story = $('.edit.details.new');
 
   if (!$story.length) {
     return setTimeout(waitForNewStory, 250);
   }
-  var $titleInput = $story.find('textarea[name="story[name]"]');
+  const $titleInput = $story.find('textarea[name="story[name]"]');
   const titleMessage = browser.i18n.getMessage('storyTitleTemplate');
   snippetInput($titleInput, titleMessage);
 
   $story.find('div[data-aid="renderedDescription"]').click(waitForDescriptionTextarea);
+};
+
+const initAddStory = () => {
+  $('a[title="Add Story"]').each(function() {
+    if ($(this).data('eventset') == undefined) {
+      $(this).data('eventset', 'true');
+      $(this).click(waitForNewStory);
+    }
+  });
+};
+
+const runAddStory = () => {
+  setInterval(initAddStory, 1000);
 }
 
-$(function() {
-  function waitForAddStoryButton() {
-    let $addStoryButton = $('a[title="Add Story"]');
-
-    if (!$addStoryButton.length) {
-      return setTimeout(waitForAddStoryButton, 250);
-    }
-
-    $addStoryButton.click(waitForNewStory);
-  }
-  waitForAddStoryButton();
-});
+export default runAddStory;
--- src/content/pivotal/js/index.js
+++ src/content/pivotal/js/index.js
@@ -1,2 +1,20 @@
-import './add_story';
-import './harvest_button';
+import $ from 'jquery';
+import runAddStory from './add_story';
+import runCommentTemplate from './comment_template';
+import runHarvestButton from './harvest_button';
+
+let waitForStoriesThenRun = () => {
+  let $stories = $('.story.model');
+
+  if (!$stories.length) {
+    return setTimeout(waitForStoriesThenRun, 250);
+  }
+
+  runHarvestButton();
+  runCommentTemplate();
+  runAddStory();
+}
+
+$(function() {
+  waitForStoriesThenRun();
+});
