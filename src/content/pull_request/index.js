1c28495a29a6ca6a7d446329bb689bbe7423c472
Paul Mesnilgrente <web@paul-mesnilgrente.com>
2020-04-03 16:10:28 +1300

    fixes the textarea in gitlab, it's now resizing on fill

--- src/content/pull_request/index.js
+++ src/content/pull_request/index.js
@@ -3,19 +3,19 @@ import browser from 'webextension-polyfill';
 
 browser.runtime.onMessage.addListener(pull_request => {
   // github
-  $('#pull_request_title').val(pull_request.title);
-  $('#pull_request_body').val(pull_request.description)
+  if ($('#pull_request_title').length) {
+    $('#pull_request_title').val(pull_request.title);
+    $('#pull_request_body').val(pull_request.description)
+  }
 
   // gitlab
-  $('#merge_request_title').val(pull_request.title);
-  $('#merge_request_description').val(pull_request.description)
+  if ($('#merge_request_title').length) {
+    $('#merge_request_title').val(pull_request.title);
+    $('#merge_request_description').val(pull_request.description)
 
-  if ($('#merge_request_description').length) {
-    // trigger a resizing of the textarea to include the whole text
-    let script = document.createElement('script');
-    script.type = 'text/javascript';
-    script.async = true;
-    script.textContent = `$('#merge_request_description').trigger('change');`;
-    $('head').append(script);
+    // https://stackoverflow.com/questions/2856513/how-can-i-trigger-an-onchange-event-manually
+    const evt = document.createEvent("HTMLEvents");
+    evt.initEvent("change", false, true);
+    $('#merge_request_description')[0].dispatchEvent(evt);
   }
 });
