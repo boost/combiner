e4bc7ad0d782858d9870b15940dd19511d272a70
Paul Mesnilgrente <web@paul-mesnilgrente.com>
2022-09-29 22:14:17 +0200

    conf: avoided a warning on package.json name not following the standard

--- utils/adjust_manifest.js
+++ utils/adjust_manifest.js
@@ -1,6 +1,6 @@
 const env = require('./env');
 
-module.exports = (content, path) => {
+module.exports = (content) => {
   const manifest = JSON.parse(content);
   if (env.NODE_ENV == 'production') {
     delete manifest.content_security_policy;
@@ -9,8 +9,12 @@ module.exports = (content, path) => {
     delete manifest.sidebar_action;
     delete manifest.commands._execute_sidebar_action;
   }
-  manifest.name = process.env.npm_package_name;
-  manifest.short_name = process.env.npm_package_name;
+
+  const name = process.env.npm_package_name.charAt(0).toUpperCase() +
+               process.env.npm_package_name.slice(1);
+
+  manifest.name = name;
+  manifest.short_name = name;
   manifest.description = process.env.npm_package_description;
   manifest.version = process.env.npm_package_version;
   manifest.author = process.env.npm_package_author;
